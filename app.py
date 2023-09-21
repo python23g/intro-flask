9b952f9bfc329c3bce5628272d4b44d0a46f4fb9
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-21 09:54:39 +0500

    route

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+@app.route('/salom')
+@app.route("/hi")
+def hi():
+    return "<h1>Hi, World!</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
