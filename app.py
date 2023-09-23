7bb1ce5c28582a5e8d4bbeca35359951846af631
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-23 17:07:22 +0500

    calculator

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Welcome!</h1>"
+    return '''<form action="http://127.0.0.1:5000/form">
+            <label>a:</label><br>
+            <input type="text"  name="a" value="0"><br>
+            <label>b:</label><br>
+            <input type="text" name="b" value="0"><br><br>
+            <label>operate:</label><br>
+            <input type="text" name="op" value="+"><br><br>
+            <input type="submit" value="Submit">
+            </form>'''
 
-@app.route("/news/")
+@app.route("/form")
 def news():
-    return "<h1>News!</h1>"
 
-@app.route("/index/")
-@app.route("/home/")
-def home():
-    return "<h1>Home!</h1>"
+    params = request.args
 
-@app.route("/contact/")
-@app.route("/about/")
-def about():
-    return "<h1>Contact!</h1>"
+    # get a and b
+    a = params.get('a')
+    b = params.get('b')
+
+    # get op
+    op = params.get('op')
+
+    if op == "+":
+        return f"<h1>{a} + {b} = {int(a) + int(b)}</h1>"
+
+    if op == "-":
+        return f"<h1>{a} - {b} = {int(a) - int(b)}</h1>"
+
+    if op == "*":
+        return f"<h1>{a} * {b} = {int(a) * int(b)}</h1>"
+
+    if op == "/":
+        if int(b) == 0:
+            return "can not divide by 0."
+        return f"<h1>{a} / {b} = {round(int(a) / int(b), 2)}</h1>"
 
 
 if __name__ == "__main__":
