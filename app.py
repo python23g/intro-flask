3989226f8bb4ecd13ad6e14f4743c2880fc9a655
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-26 10:41:56 +0500

    users in templates

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
 from flask import Flask, request, render_template
+import requests
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+def get_users(n: int):
+    url = 'https://randomuser.me/api/'
+    payload = {
+        'results': n
+    }
 
-    return render_template('index.html')
+    r = requests.get(url, params=payload)
+
+    users = []
+    n = 1
+    for user in r.json()['results']:
+        users.append({
+            'id': n,
+            'first': user['name']['first'],
+            'last': user['name']['last'],
+            'age': user['dob']['age'],
+            'gender': user['gender'],
+            'email': user['email'],
+            'location': user['location']['country'],
+            'link': user['picture']['large'],
+        })
+        n += 1
+
+    return users
+
+
+@app.route("/users")
+def index():
+    title = 'Random Users'
+
+    n = request.args.get('n', 1)
+
+    context = {
+        'title': title,
+        'users': get_users(int(n))
+    }
+
+    return render_template('index.html', context=context)
 
 
 
